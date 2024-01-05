c398aa5100a90b423e9834d00c491dd0725bf824
lauax <lucas12alfredsson@gmail.com>
2024-01-06 00:06:21 +0100

    PastExperiences

---
+++ My-CV/src/assets/Pages/PastExperiences.tsx
@@ -0,0 +1,11 @@
+import { Flex } from "@chakra-ui/react";
+
+function PastExperiences() {
+  return (
+    <Flex px={35} flexDirection="column" gap={10}>
+
+    </Flex>
+  );
+}
+
+export default PastExperiences;
