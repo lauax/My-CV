2b5e25e75051503767b8b85bdb02cf7f9fea1c6f
lauax <lucas12alfredsson@gmail.com>
2024-01-05 23:55:39 +0100

    Kör utan carousel istället en stor bra bild för varje project i mitt portfolio

---
+++ My-CV/src/assets/Components/HeroImage.tsx
@@ -0,0 +1,43 @@
+import { Box, Flex, Heading, Link, Text } from "@chakra-ui/react";
+import React from 'react';
+
+interface HeroImageProps {
+  imageUrl: string;
+  title: string;
+  description: string;
+  link: string;
+}
+
+const HeroImage: React.FC<HeroImageProps> = ({
+  imageUrl,
+  title,
+  description,
+  link
+}) => {
+  return (
+    <Flex
+      w="full"
+      h="70vh"
+      justifyContent="center"
+      alignItems="center"
+      bgImage={`url(${imageUrl})`}
+      bgSize="cover"
+      bgPos="center"
+      color="white"
+    >
+      <Box pl={{ base: "4", md: "10" }} textAlign="center">
+        <Heading fontSize={{ base: "30", md: "52" }} mb={4}>
+          {title}
+        </Heading>
+        <Text mb={6} fontSize={{ base: "18", md: "22" }}>
+          {description}
+        </Text>
+        <Link href={link}>
+          View more
+        </Link>
+      </Box>
+    </Flex>
+  );
+}
+
+export default HeroImage;
